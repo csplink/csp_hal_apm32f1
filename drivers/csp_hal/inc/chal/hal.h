b8d19f181d497385bce029e43a465b8de92c03e4
小权一句两句 <xqyjlj@126.com>
2022-08-08 22:54:44 +0800

    添加其他模块

--- drivers/csp_hal/inc/chal/hal.h
+++ drivers/csp_hal/inc/chal/hal.h
@@ -19,8 +19,32 @@
 #define __CHAL_HAL_H__
 
 #include <apm32f10x.h>
+#include <apm32f10x_adc.h>
+#include <apm32f10x_bakpr.h>
+#include <apm32f10x_can.h>
+#include <apm32f10x_crc.h>
+#include <apm32f10x_dac.h>
+#include <apm32f10x_dbgmcu.h>
+#include <apm32f10x_dma.h>
+#include <apm32f10x_dmc.h>
+#include <apm32f10x_eint.h>
+#include <apm32f10x_emmc.h>
+#include <apm32f10x_fmc.h>
 #include <apm32f10x_gpio.h>
+#include <apm32f10x_i2c.h>
+#include <apm32f10x_iwdt.h>
+#include <apm32f10x_misc.h>
+#include <apm32f10x_pmu.h>
+#include <apm32f10x_qspi.h>
 #include <apm32f10x_rcm.h>
+#include <apm32f10x_rtc.h>
+#include <apm32f10x_sci2c.h>
+#include <apm32f10x_sdio.h>
+#include <apm32f10x_spi.h>
+#include <apm32f10x_tmr.h>
+#include <apm32f10x_usart.h>
+#include <apm32f10x_wwdt.h>
+
 #include <rtthread.h>
 
 #ifdef __cplusplus
