25f61afb694ffc730cf89bc557d40dcdcd725566
小权一句两句 <xqyjlj@126.com>
2022-09-14 23:32:28 +0800

    更新脚本

--- description/tools/svg_to_position.py
+++ description/tools/svg_to_position.py
@@ -18,33 +18,55 @@ class Rect:
 
 class Svg:
     @staticmethod
-    def generate(path):
-        tree = Et.parse(path)
-        root = tree.getroot()
+    def generate_xml(file, width, height, rects):
+        xml_clock = Et.Element('Clock')
+        xml_rects = Et.SubElement(xml_clock, 'Rects', Width=str(width), Height=str(height))
+        i = 0
+        for item in rects:
+            i += 1
+            Et.SubElement(xml_rects, 'Rect', ID=str(i), X=str(item.x), Y=str(item.y), Width=str(item.width),
+                          Height=str(item.height))
+        dom = minidom.parseString(Et.tostring(xml_clock))
+
+        with open(file, 'w') as f:
+            dom.writexml(f, "", "    ", "\n", "utf-8")
+
+    @staticmethod
+    def parse_svg(file):
         rects = []
         width = 0
         height = 0
-        for item in root.iter():
+
+        svg_tree = Et.parse(file)
+        svg_root = svg_tree.getroot()
+        for item in svg_root.iter():
             if item.tag.lower().endswith("svg"):
                 width = float(item.attrib["width"].replace("px", ""))
                 height = float(item.attrib["height"].replace("px", ""))
             elif item.tag.lower().endswith("rect") and "ff0000" in item.attrib["stroke"].lower():
                 rects.append(Rect(item.attrib["x"], item.attrib["y"], item.attrib["width"], item.attrib["height"]))
-        rects.sort(key=lambda x: (x.x, x.y))
-        xml_rects = Et.Element('Rects', width=str(width), height=str(height))
-        i = 0
-        for item in rects:
-            i += 1
-            Et.SubElement(xml_rects, 'Rect', id=str(i), x=str(item.x), y=str(item.y), width=str(item.width),
-                          height=str(item.height))
-        dom = minidom.parseString(Et.tostring(xml_rects))
-        file_path, file_name = os.path.split(path)
-        file_path = file_path + "/" + os.path.splitext(file_name)[0] + ".xml"
-        print("generate: " + file_path)
-        with open(file_path, 'w') as f:
-            dom.writexml(f, "", "    ", "\n", "utf-8")
+        rects.sort(key=lambda x: (x.x, x.y))  # 按坐标重排控件
+        return width, height, rects
+
+    @staticmethod
+    def generate(file):
+        width, height, rects = Svg.parse_svg(file)  # 解析svg文件
+        # 获取xml路径
+        file_path, file_name = os.path.split(file)
+        xml_path = file_path + "/" + os.path.splitext(file_name)[0] + ".xml"
+
+        # 不存在则直接新建一个
+        if not os.path.exists(xml_path):
+            Svg.generate_xml(xml_path, width, height, rects)
+        else:
+            print("generate: " + xml_path)
+
+        print("generate: " + xml_path)
 
 
 if __name__ == "__main__":
-    Svg.generate(
-        r"D:\Users\xqyjlj\Documents\git\github\xqyjlj\csp_hal_apm32f1\description\apm32f103zet6\clock\apm32f103zet6.svg")
+    description_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    for name in os.listdir(description_path):
+        path = description_path + "/" + name + "/clock/" + name + ".svg"
+        if os.path.exists(path):
+            Svg.generate(path)
