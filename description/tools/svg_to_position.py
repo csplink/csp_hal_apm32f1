e81485d4ac64b29311ae2f0ddcac8987450bcba1
小权一句两句 <xqyjlj@126.com>
2022-09-18 19:05:41 +0800

    添加圆圈生成代码

--- description/tools/svg_to_position.py
+++ description/tools/svg_to_position.py
@@ -15,6 +15,19 @@ class Rect:
         self.height = float(height) + 1
 
 
+class Ellipse:
+    x = 0
+    y = 0
+    width = 0
+    height = 0
+
+    def __init__(self, x, y, width, height):
+        self.x = float(x) - 0.5
+        self.y = float(y) - 0.5
+        self.width = float(width) + 1
+        self.height = float(height) + 1
+
+
 class Svg:
     @staticmethod
     def indent(elem, level=0):
@@ -33,20 +46,28 @@ class Svg:
                 elem.tail = i
 
     @staticmethod
-    def generate_xml(file, width, height, rects):
+    def generate_xml(file, width, height, rects, ellipses):
         xml_clock = Et.Element('Clock', Width=str(width), Height=str(height))
+
         xml_rects = Et.SubElement(xml_clock, 'Rects')
         i = 0
         for item in rects:
             i += 1
             Et.SubElement(xml_rects, 'Rect', ID=str(i), X=str(item.x), Y=str(item.y), Width=str(item.width),
                           Height=str(item.height))
+
+        xml_rects = Et.SubElement(xml_clock, 'Ellipses')
+        for item in ellipses:
+            i += 1
+            Et.SubElement(xml_rects, 'Ellipse', ID=str(i), X=str(item.x), Y=str(item.y), Width=str(item.width),
+                          Height=str(item.height))
+
         Svg.indent(xml_clock)
         with open(file, 'wb') as f:
             f.write(Et.tostring(xml_clock, encoding="utf8"))
 
     @staticmethod
-    def change_xml(file, width, height, rects):
+    def change_xml(file, width, height, rects, ellipses):
         xml_tree = Et.parse(file)
         xml_root = xml_tree.getroot()
 
@@ -54,9 +75,12 @@ class Svg:
             xml_root.attrib["Width"] = str(width)
             xml_root.attrib["Height"] = str(height)
 
-        # 只删除Rects节点
+        # 只删除Rects与Ellipses节点
         rect_nodes = xml_root.findall("Rects")
         for node in rect_nodes:
+            xml_root.remove(node)
+        ellipse_nodes = xml_root.findall("Ellipses")
+        for node in ellipse_nodes:
             xml_root.remove(node)
 
         xml_rects = Et.Element('Rects')
@@ -66,6 +90,14 @@ class Svg:
             Et.SubElement(xml_rects, 'Rect', ID=str(i), X=str(item.x), Y=str(item.y), Width=str(item.width),
                           Height=str(item.height))
         xml_root.append(xml_rects)
+
+        xml_ellipses = Et.Element('Ellipses')
+        for item in ellipses:
+            i += 1
+            Et.SubElement(xml_ellipses, 'Ellipse', ID=str(i), X=str(item.x), Y=str(item.y), Width=str(item.width),
+                          Height=str(item.height))
+        xml_root.append(xml_ellipses)
+
         Svg.indent(xml_root)
         with open(file, 'wb') as f:
             f.write(Et.tostring(xml_root, encoding="utf8"))
@@ -73,6 +105,7 @@ class Svg:
     @staticmethod
     def parse_svg(file):
         rects = []
+        ellipses = []
         width = 0
         height = 0
 
@@ -92,21 +125,28 @@ class Svg:
                     if item.attrib["stroke-width"] == "1":
                         rects.append(
                             Rect(item.attrib["x"], item.attrib["y"], item.attrib["width"], item.attrib["height"]))
-        rects.sort(key=lambda x: (x.x, x.y))  # 按坐标重排控件
-        return width, height, rects
+            # rx 和 ry 为的半径
+            elif item.tag.lower().endswith("ellipse"):
+                if item.attrib["rx"] == item.attrib["ry"]:
+                    if float(item.attrib["rx"]) >= 5:
+                        ellipses.append(
+                            Ellipse(item.attrib["cx"], item.attrib["cy"], item.attrib["rx"], item.attrib["ry"]))
+            rects.sort(key=lambda x: (x.x, x.y))  # 按坐标重排控件
+            ellipses.sort(key=lambda x: (x.x, x.y))  # 按坐标重排控件
+        return width, height, rects, ellipses
 
     @staticmethod
     def generate(file):
-        width, height, rects = Svg.parse_svg(file)  # 解析svg文件
+        width, height, rects, ellipses = Svg.parse_svg(file)  # 解析svg文件
         # 获取xml路径
         file_path, file_name = os.path.split(file)
         xml_path = file_path + "/" + os.path.splitext(file_name)[0] + ".xml"
 
         # 不存在则直接新建一个
         if not os.path.exists(xml_path):
-            Svg.generate_xml(xml_path, width, height, rects)
+            Svg.generate_xml(xml_path, width, height, rects, ellipses)
         else:
-            Svg.change_xml(xml_path, width, height, rects)
+            Svg.change_xml(xml_path, width, height, rects, ellipses)
 
         print("generate: " + xml_path)
 
