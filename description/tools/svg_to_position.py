0391a4e358807cc1daf25ea3e003100f8c185f94
小权一句两句 <xqyjlj@126.com>
2022-09-24 18:10:05 +0800

    原有编码格式错误，已修复

--- description/tools/svg_to_position.py
+++ description/tools/svg_to_position.py
@@ -64,7 +64,7 @@ class Svg:
 
         Svg.indent(xml_clock)
         with open(file, 'wb') as f:
-            f.write(Et.tostring(xml_clock, encoding="utf8"))
+            f.write(Et.tostring(xml_clock, encoding="utf-8", xml_declaration=True))
 
     @staticmethod
     def change_xml(file, width, height, rects, ellipses):
@@ -100,7 +100,7 @@ class Svg:
 
         Svg.indent(xml_root)
         with open(file, 'wb') as f:
-            f.write(Et.tostring(xml_root, encoding="utf8"))
+            f.write(Et.tostring(xml_root, encoding="utf-8", xml_declaration=True))
 
     @staticmethod
     def parse_svg(file):
