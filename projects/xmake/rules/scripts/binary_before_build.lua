5933394b945b218a6dddd66621f2589dd00c5ff9
xqyjlj <xqyjlj@126.com>
2022-11-13 23:56:26 +0800

    add map generate

--- projects/xmake/rules/scripts/binary_before_build.lua
+++ projects/xmake/rules/scripts/binary_before_build.lua
@@ -1,1 +1,9 @@
-function main(target) end
+import("core.project.config")
+import("core.project.project")
+
+local artifact_dir = config.buildir() .. "/artifact/" .. config.mode() .. "/"
+
+function main(target)
+    target:add("ldflags", "-Wl,--print-memory-usage,-Map=" .. artifact_dir ..
+                   project.name() .. ".map")
+end
