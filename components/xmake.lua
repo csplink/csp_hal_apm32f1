323c903d2cf6a05cfb083c3a69fa63f1b4b18769
xqyjlj <xqyjlj@126.com>
2022-11-20 14:39:46 +0800

    format

--- components/xmake.lua
+++ components/xmake.lua
@@ -20,8 +20,8 @@
 
 set_xmakever("2.7.2")
 
-for _, dir in ipairs(os.dirs(os.scriptdir().."/*")) do
-    if os.exists(dir.."/xmake.lua") then
+for _, dir in ipairs(os.dirs(os.scriptdir() .. "/*")) do
+    if os.exists(dir .. "/xmake.lua") then
         includes(dir)
     end
 end
