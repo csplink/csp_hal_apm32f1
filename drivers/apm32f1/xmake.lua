5e0c26ca3b304935a5f66750f1d0618a2045015e
xqyjlj <xqyjlj@126.com>
2022-11-20 14:38:09 +0800

    format

--- drivers/apm32f1/xmake.lua
+++ drivers/apm32f1/xmake.lua
@@ -21,24 +21,17 @@
 set_xmakever("2.7.2")
 
 target("drivers_hal")
+do
     set_kind("static")
-    add_includedirs(
-        "cmsis/Include",
-        "hal/Inc",
-        "cmsis_core/Include",
-        "$(buildir)",
-        {public = true}
-    )
-    add_files(
-        "cmsis/Source/Templates/system_stm32f1xx.c",
-        "hal/Src/*.c"
-    )
-    remove_files(
-        "hal/Src/*_template.c"
-    )
-    if(is_config("CSP_MCU", "apm32f103zet6")) then
-        if(is_config("compiler", "gcc")) then
+    add_includedirs("cmsis/Include", "hal/Inc", "cmsis_core/Include", "$(buildir)", {public = true})
+    add_files("cmsis/Source/Templates/system_stm32f1xx.c", "hal/Src/*.c")
+    remove_files("hal/Src/*_template.c")
+
+    if (is_config("CSP_MCU", "apm32f103zet6")) then
+        if (is_config("compiler", "gcc")) then
             add_files("cmsis/Source/Templates/gcc/startup_stm32f103xe.s")
         end
         add_defines("STM32F103xE")
     end
+end
+target_end()
--- drivers/csp_hal/xmake.lua
+++ drivers/csp_hal/xmake.lua
@@ -21,14 +21,9 @@
 set_xmakever("2.7.2")
 
 target("drivers_csp")
-	set_kind("static")
-	add_files(
-        "chal/src/*.c",
-        "port/src/*.c"
-    )
-    add_includedirs(
-        "chal/inc",
-        "port/inc",
-        "$(buildir)",
-        {public = true}
-    )
+do
+    set_kind("static")
+    add_files("chal/src/*.c", "port/src/*.c")
+    add_includedirs("chal/inc", "port/inc", "$(buildir)", {public = true})
+end
+target_end()
