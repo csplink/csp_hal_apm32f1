c6b2745b8e8ee6d9be7e8d99807a3018e90579c0
xqyjlj <xqyjlj@126.com>
2022-11-29 00:19:17 +0800

    🐞 fix(drivers/csp_hal/chal/inc/chal): add some code for gpio

--- drivers/csp_hal/chal/inc/chal/gpio.h
+++ drivers/csp_hal/chal/inc/chal/gpio.h
@@ -1,17 +1,21 @@
-/**
- * @file gpio.h
- * @author xqyjlj (xqyjlj@126.com)
- * @brief
- * @version 0.1
- * @date 2022-07-27
+/* header for chal_gpio.c.
  *
- * @copyright Copyright (c) 2022-2022, xqyjlj
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @SPDX-License-Identifier: Apache-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Change Logs:
- * Date           Author       Notes
- * 2022-07-27     xqyjlj       the first version
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2022-present xqyjlj<xqyjlj@126.com>, csplink.github.io
+ *
+ * @author      xqyjlj
+ * @file        gpio.h
  *
  */
 
@@ -23,6 +27,185 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/**
+ * @brief CHAL_GPIO_GET_PIN(A, 0) = GPIOA, LL_GPIO_PIN_0
+ */
+#define CHAL_GPIO_GET_PIN(PORTx, PIN) GPIO##PORTx, LL_GPIO_PIN_##PIN
+
+#if defined(GPIOA)
+#define CHAL_PA0            CHAL_GPIO_GET_PIN(A, 0)
+#define CHAL_PA1            CHAL_GPIO_GET_PIN(A, 1)
+#define CHAL_PA2            CHAL_GPIO_GET_PIN(A, 2)
+#define CHAL_PA3            CHAL_GPIO_GET_PIN(A, 3)
+#define CHAL_PA4            CHAL_GPIO_GET_PIN(A, 4)
+#define CHAL_PA5            CHAL_GPIO_GET_PIN(A, 5)
+#define CHAL_PA6            CHAL_GPIO_GET_PIN(A, 6)
+#define CHAL_PA7            CHAL_GPIO_GET_PIN(A, 7)
+#define CHAL_PA8            CHAL_GPIO_GET_PIN(A, 8)
+#define CHAL_PA9            CHAL_GPIO_GET_PIN(A, 9)
+#define CHAL_PA10           CHAL_GPIO_GET_PIN(A, 10)
+#define CHAL_PA11           CHAL_GPIO_GET_PIN(A, 11)
+#define CHAL_PA12           CHAL_GPIO_GET_PIN(A, 12)
+#define CHAL_PA13           CHAL_GPIO_GET_PIN(A, 13)
+#define CHAL_PA14           CHAL_GPIO_GET_PIN(A, 14)
+#define CHAL_PA15           CHAL_GPIO_GET_PIN(A, 15)
+#define CHAL_GPIO_CLK_GPIOA LL_APB2_GRP1_PERIPH_GPIOA
+#endif
+
+#if defined(GPIOB)
+#define CHAL_PB0            CHAL_GPIO_GET_PIN(B, 0)
+#define CHAL_PB1            CHAL_GPIO_GET_PIN(B, 1)
+#define CHAL_PB2            CHAL_GPIO_GET_PIN(B, 2)
+#define CHAL_PB3            CHAL_GPIO_GET_PIN(B, 3)
+#define CHAL_PB4            CHAL_GPIO_GET_PIN(B, 4)
+#define CHAL_PB5            CHAL_GPIO_GET_PIN(B, 5)
+#define CHAL_PB6            CHAL_GPIO_GET_PIN(B, 6)
+#define CHAL_PB7            CHAL_GPIO_GET_PIN(B, 7)
+#define CHAL_PB8            CHAL_GPIO_GET_PIN(B, 8)
+#define CHAL_PB9            CHAL_GPIO_GET_PIN(B, 9)
+#define CHAL_PB10           CHAL_GPIO_GET_PIN(B, 10)
+#define CHAL_PB11           CHAL_GPIO_GET_PIN(B, 11)
+#define CHAL_PB12           CHAL_GPIO_GET_PIN(B, 12)
+#define CHAL_PB13           CHAL_GPIO_GET_PIN(B, 13)
+#define CHAL_PB14           CHAL_GPIO_GET_PIN(B, 14)
+#define CHAL_PB15           CHAL_GPIO_GET_PIN(B, 15)
+#define CHAL_GPIO_CLK_GPIOB LL_APB2_GRP1_PERIPH_GPIOB
+#endif
+
+#if defined(GPIOC)
+#define CHAL_PC0            CHAL_GPIO_GET_PIN(C, 0)
+#define CHAL_PC1            CHAL_GPIO_GET_PIN(C, 1)
+#define CHAL_PC2            CHAL_GPIO_GET_PIN(C, 2)
+#define CHAL_PC3            CHAL_GPIO_GET_PIN(C, 3)
+#define CHAL_PC4            CHAL_GPIO_GET_PIN(C, 4)
+#define CHAL_PC5            CHAL_GPIO_GET_PIN(C, 5)
+#define CHAL_PC6            CHAL_GPIO_GET_PIN(C, 6)
+#define CHAL_PC7            CHAL_GPIO_GET_PIN(C, 7)
+#define CHAL_PC8            CHAL_GPIO_GET_PIN(C, 8)
+#define CHAL_PC9            CHAL_GPIO_GET_PIN(C, 9)
+#define CHAL_PC10           CHAL_GPIO_GET_PIN(C, 10)
+#define CHAL_PC11           CHAL_GPIO_GET_PIN(C, 11)
+#define CHAL_PC12           CHAL_GPIO_GET_PIN(C, 12)
+#define CHAL_PC13           CHAL_GPIO_GET_PIN(C, 13)
+#define CHAL_PC14           CHAL_GPIO_GET_PIN(C, 14)
+#define CHAL_PC15           CHAL_GPIO_GET_PIN(C, 15)
+#define CHAL_GPIO_CLK_GPIOC LL_APB2_GRP1_PERIPH_GPIOC
+#endif
+
+#if defined(GPIOD)
+#define CHAL_PD0            CHAL_GPIO_GET_PIN(D, 0)
+#define CHAL_PD1            CHAL_GPIO_GET_PIN(D, 1)
+#define CHAL_PD2            CHAL_GPIO_GET_PIN(D, 2)
+#define CHAL_PD3            CHAL_GPIO_GET_PIN(D, 3)
+#define CHAL_PD4            CHAL_GPIO_GET_PIN(D, 4)
+#define CHAL_PD5            CHAL_GPIO_GET_PIN(D, 5)
+#define CHAL_PD6            CHAL_GPIO_GET_PIN(D, 6)
+#define CHAL_PD7            CHAL_GPIO_GET_PIN(D, 7)
+#define CHAL_PD8            CHAL_GPIO_GET_PIN(D, 8)
+#define CHAL_PD9            CHAL_GPIO_GET_PIN(D, 9)
+#define CHAL_PD10           CHAL_GPIO_GET_PIN(D, 10)
+#define CHAL_PD11           CHAL_GPIO_GET_PIN(D, 11)
+#define CHAL_PD12           CHAL_GPIO_GET_PIN(D, 12)
+#define CHAL_PD13           CHAL_GPIO_GET_PIN(D, 13)
+#define CHAL_PD14           CHAL_GPIO_GET_PIN(D, 14)
+#define CHAL_PD15           CHAL_GPIO_GET_PIN(D, 15)
+#define CHAL_GPIO_CLK_GPIOD LL_APB2_GRP1_PERIPH_GPIOD
+#endif
+
+#if defined(GPIOE)
+#define CHAL_PE0            CHAL_GPIO_GET_PIN(E, 0)
+#define CHAL_PE1            CHAL_GPIO_GET_PIN(E, 1)
+#define CHAL_PE2            CHAL_GPIO_GET_PIN(E, 2)
+#define CHAL_PE3            CHAL_GPIO_GET_PIN(E, 3)
+#define CHAL_PE4            CHAL_GPIO_GET_PIN(E, 4)
+#define CHAL_PE5            CHAL_GPIO_GET_PIN(E, 5)
+#define CHAL_PE6            CHAL_GPIO_GET_PIN(E, 6)
+#define CHAL_PE7            CHAL_GPIO_GET_PIN(E, 7)
+#define CHAL_PE8            CHAL_GPIO_GET_PIN(E, 8)
+#define CHAL_PE9            CHAL_GPIO_GET_PIN(E, 9)
+#define CHAL_PE10           CHAL_GPIO_GET_PIN(E, 10)
+#define CHAL_PE11           CHAL_GPIO_GET_PIN(E, 11)
+#define CHAL_PE12           CHAL_GPIO_GET_PIN(E, 12)
+#define CHAL_PE13           CHAL_GPIO_GET_PIN(E, 13)
+#define CHAL_PE14           CHAL_GPIO_GET_PIN(E, 14)
+#define CHAL_PE15           CHAL_GPIO_GET_PIN(E, 15)
+#define CHAL_GPIO_CLK_GPIOE LL_APB2_GRP1_PERIPH_GPIOE
+#endif
+
+#if defined(GPIOF)
+#define CHAL_PF0            CHAL_GPIO_GET_PIN(F, 0)
+#define CHAL_PF1            CHAL_GPIO_GET_PIN(F, 1)
+#define CHAL_PF2            CHAL_GPIO_GET_PIN(F, 2)
+#define CHAL_PF3            CHAL_GPIO_GET_PIN(F, 3)
+#define CHAL_PF4            CHAL_GPIO_GET_PIN(F, 4)
+#define CHAL_PF5            CHAL_GPIO_GET_PIN(F, 5)
+#define CHAL_PF6            CHAL_GPIO_GET_PIN(F, 6)
+#define CHAL_PF7            CHAL_GPIO_GET_PIN(F, 7)
+#define CHAL_PF8            CHAL_GPIO_GET_PIN(F, 8)
+#define CHAL_PF9            CHAL_GPIO_GET_PIN(F, 9)
+#define CHAL_PF10           CHAL_GPIO_GET_PIN(F, 10)
+#define CHAL_PF11           CHAL_GPIO_GET_PIN(F, 11)
+#define CHAL_PF12           CHAL_GPIO_GET_PIN(F, 12)
+#define CHAL_PF13           CHAL_GPIO_GET_PIN(F, 13)
+#define CHAL_PF14           CHAL_GPIO_GET_PIN(F, 14)
+#define CHAL_PF15           CHAL_GPIO_GET_PIN(F, 15)
+#define CHAL_GPIO_CLK_GPIOF LL_APB2_GRP1_PERIPH_GPIOF
+#endif
+
+#if defined(GPIOG)
+#define CHAL_PG0            CHAL_GPIO_GET_PIN(G, 0)
+#define CHAL_PG1            CHAL_GPIO_GET_PIN(G, 1)
+#define CHAL_PG2            CHAL_GPIO_GET_PIN(G, 2)
+#define CHAL_PG3            CHAL_GPIO_GET_PIN(G, 3)
+#define CHAL_PG4            CHAL_GPIO_GET_PIN(G, 4)
+#define CHAL_PG5            CHAL_GPIO_GET_PIN(G, 5)
+#define CHAL_PG6            CHAL_GPIO_GET_PIN(G, 6)
+#define CHAL_PG7            CHAL_GPIO_GET_PIN(G, 7)
+#define CHAL_PG8            CHAL_GPIO_GET_PIN(G, 8)
+#define CHAL_PG9            CHAL_GPIO_GET_PIN(G, 9)
+#define CHAL_PG10           CHAL_GPIO_GET_PIN(G, 10)
+#define CHAL_PG11           CHAL_GPIO_GET_PIN(G, 11)
+#define CHAL_PG12           CHAL_GPIO_GET_PIN(G, 12)
+#define CHAL_PG13           CHAL_GPIO_GET_PIN(G, 13)
+#define CHAL_PG14           CHAL_GPIO_GET_PIN(G, 14)
+#define CHAL_PG15           CHAL_GPIO_GET_PIN(G, 15)
+#define CHAL_GPIO_CLK_GPIOG LL_APB2_GRP1_PERIPH_GPIOG
+#endif
+
+#define CHAL_GPIO_CLK_AFIO LL_APB2_GRP1_PERIPH_AFIO
+
+/**
+ * @brief GPIO level low and high enumeration
+ */
+typedef enum
+{
+    CHAL_GPIO_LEVEL_LOW = 0u,
+    CHAL_GPIO_LEVEL_HIGH
+} chal_gpio_level;
+
+__chal_inline void chal_gpio_enable_clk(uint32_t periphs)
+{
+    LL_APB2_GRP1_EnableClock(periphs);
+}
+
+__chal_inline uint32_t chal_gpio_is_clk_enabled(uint32_t periphs)
+{
+    return LL_APB2_GRP1_IsEnabledClock(periphs);
+}
+
+__chal_inline void chal_gpio_disable_clk(uint32_t periphs)
+{
+    LL_APB2_GRP1_DisableClock(periphs);
+}
+
+__chal_inline void chal_gpio_set_level(GPIO_TypeDef *gpiox, uint32_t pinmask, chal_gpio_level level)
+{
+    if (level == CHAL_GPIO_LEVEL_LOW)
+        LL_GPIO_ResetOutputPin(gpiox, pinmask);
+    else
+        LL_GPIO_SetOutputPin(gpiox, pinmask);
+}
 
 #ifdef __cplusplus
 }
--- drivers/csp_hal/chal/inc/chal/hal.h
+++ drivers/csp_hal/chal/inc/chal/hal.h
@@ -1,26 +1,56 @@
-/**
- * @file hal.h
- * @author xqyjlj (xqyjlj@126.com)
- * @brief
- * @version 0.1
- * @date 2022-07-27
+/* header for csp_hal library.
  *
- * @copyright Copyright (c) 2022-2022, xqyjlj
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @SPDX-License-Identifier: Apache-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Change Logs:
- * Date           Author       Notes
- * 2022-07-27     xqyjlj       the first version
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2022-present xqyjlj<xqyjlj@126.com>, csplink.github.io
+ *
+ * @author      xqyjlj
+ * @file        hal.h
  *
  */
 
 #ifndef __CHAL_HAL_H__
 #define __CHAL_HAL_H__
 
+#include "stm32f1xx_ll_adc.h"
+#include "stm32f1xx_ll_bus.h"
+#include "stm32f1xx_ll_cortex.h"
+#include "stm32f1xx_ll_crc.h"
+#include "stm32f1xx_ll_dac.h"
+#include "stm32f1xx_ll_dma.h"
+#include "stm32f1xx_ll_exti.h"
+#include "stm32f1xx_ll_fsmc.h"
+#include "stm32f1xx_ll_gpio.h"
+#include "stm32f1xx_ll_i2c.h"
+#include "stm32f1xx_ll_iwdg.h"
+#include "stm32f1xx_ll_pwr.h"
+#include "stm32f1xx_ll_rcc.h"
+#include "stm32f1xx_ll_rtc.h"
+#include "stm32f1xx_ll_sdmmc.h"
+#include "stm32f1xx_ll_spi.h"
+#include "stm32f1xx_ll_system.h"
+#include "stm32f1xx_ll_tim.h"
+#include "stm32f1xx_ll_usart.h"
+#include "stm32f1xx_ll_usb.h"
+#include "stm32f1xx_ll_utils.h"
+#include "stm32f1xx_ll_wwdg.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define __chal_inline __STATIC_INLINE
+#define __chal_weak   __weak
 
 #ifdef __cplusplus
 }
