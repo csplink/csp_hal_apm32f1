f6e1a7024e5cbbd750310813e69b6dd5ba91e46c
xqyjlj <xqyjlj@126.com>
2022-11-30 09:51:59 +0800

    🌈 style(drivers/csp_hal/chal): chang header code

--- drivers/csp_hal/chal/inc/chal/flash.h
+++ drivers/csp_hal/chal/inc/chal/flash.h
@@ -1,19 +1,24 @@
-/**
- * @file flash.h
- * @author xqyjlj (xqyjlj@126.com)
- * @brief
- * @version 0.1
- * @date 2022-08-08
+/* header for chal_flash.c.
  *
- * @copyright Copyright (c) 2022-2022, xqyjlj
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @SPDX-License-Identifier: Apache-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Change Logs:
- * Date           Author       Notes
- * 2022-08-08     xqyjlj       the first version
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2022-present xqyjlj<xqyjlj@126.com>, csplink.github.io
+ *
+ * @author      xqyjlj
+ * @file        flash.h
  *
  */
+
 #ifndef __CHAL_FLASH_H__
 #define __CHAL_FLASH_H__
 
