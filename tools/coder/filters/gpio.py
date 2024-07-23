705a2a6c04215c5094fbed5c50f344ba3b54fb6e
xqyjlj <xqyjlj@126.com>
2024-07-23 08:35:52 +0800

    暂存
    Signed-off-by: xqyjlj <xqyjlj@126.com>

--- tools/coder/filters/gpio.py
+++ tools/coder/filters/gpio.py
@@ -27,20 +27,26 @@
 import json
 
 
-def _hash_dict(dictionary):
+def __hash_dict(dictionary):
     json_str = json.dumps(dictionary, sort_keys=True)
     return hash(json_str)
 
 
+def __pin_config(project: dict) -> dict:
+    return project["config"]["pin"]
+
+
+def __gpio_config(project: dict) -> dict:
+    return project["config"]["GPIO"]
+
+
 def gpio_pin_names(project: dict) -> list:
     pins = []
-
-    for key, value in project["Config"]["GPIO"].items():
-        if value["Locked"] == True and len(value["FunctionProperty"]) != 0:
+    for key, value in __pin_config(project).items():
+        if value.get("locked", False) == True and len(value.get("signal", "")) != 0:
             pins.append(key)
 
     pins.sort()
-
     return pins
 
 
@@ -50,7 +56,7 @@ def gpio_clocks(project: dict) -> list:
     pins = gpio_pin_names(project)
     for pin in pins:
         clocks.append(f"RCM_APB2_PERIPH_GPIO{str(pin[1]).upper()}")
-        if project["Config"]["GPIO"][pin]["Function"] == "GPIO-EXTI":
+        if __pin_config(project)[pin].get("signal", "") == "GPIO-EXTI":
             clocks.append(f"RCM_APB2_PERIPH_AFIO")
 
     clocks = list(set(clocks))
@@ -105,9 +111,9 @@ def gpio_pin_groups_by_port(project: dict, port: str) -> list:
     for pin in pins:
         config = {}
         if port[4] == pin[1]:
-            config["Function"] = project["Config"]["GPIO"][pin]["Function"]
-            config["Config"] = project["Config"]["GPIO"][pin]["FunctionProperty"]["GPIO"]
-            hash_str = _hash_dict(config)
+            config["signal"] = __pin_config(project)[pin].get("signal", "")
+            config["config"] = __gpio_config(project)[pin]
+            hash_str = __hash_dict(config)
             if not hash_str in pin_groups_map:
                 pin_groups_map[hash_str] = [f"GPIO_PIN_{pin[2:]}"]
             else:
@@ -125,7 +131,7 @@ def gpio_pin_groups_by_port(project: dict, port: str) -> list:
 
 def gpio_pin_alias(project: dict, port: str, pin: str) -> bool:
     pin_name = f'P{port.replace("GPIO", "")}{pin.replace("GPIO_PIN_", "")}'
-    alias = project["Config"]["GPIO"][pin_name]["Comment"]
+    alias = __pin_config(project)[pin_name].get("label", None)
 
     if alias == "":
         return None
@@ -135,17 +141,17 @@ def gpio_pin_alias(project: dict, port: str, pin: str) -> bool:
 
 def gpio_pin_level(project: dict, port: str, pin: str) -> bool:
     level_map = {
-        "std.gpio_level_low": "BIT_RESET",
-        "std.gpio_level_high": "BIT_SET",
+        "geehy.gpio_level_low": "BIT_RESET",
+        "geehy.gpio_level_high": "BIT_SET",
     }
 
     pin_name = f'P{port.replace("GPIO", "")}{pin.replace("GPIO_PIN_", "")}'
-    gpio_config = project["Config"]["GPIO"][pin_name]["FunctionProperty"]["GPIO"]
+    gpio_config = __gpio_config(project).get(pin_name, {})
 
-    if not "std.gpio_level_t" in gpio_config:
+    if not "geehy.gpio_level_t" in gpio_config:
         return None
 
-    return level_map[gpio_config["std.gpio_level_t"]]
+    return level_map[gpio_config["geehy.gpio_level_t"]]
 
 
 def gpio_pin_level_map(project: dict, port: str, pins: list) -> bool:
@@ -169,32 +175,32 @@ def gpio_pin_level_map(project: dict, port: str, pins: list) -> bool:
 
 def gpio_pin_mode(project: dict, port: str, pin: str) -> bool:
     output_mode_map = {
-        "std.gpio_output_pp": "GPIO_MODE_OUT_PP",
-        "std.gpio_output_od": "GPIO_MODE_OUT_OD",
+        "geehy.gpio_output_pp": "GPIO_MODE_OUT_PP",
+        "geehy.gpio_output_od": "GPIO_MODE_OUT_OD",
     }
     input_mode_map = {
-        "std.gpio_pull_up": "GPIO_MODE_IN_PU",
-        "std.gpio_pull_down": "GPIO_MODE_IN_PD",
-        "std.gpio_pull_no": "GPIO_MODE_IN_FLOATING",
+        "geehy.gpio_pull_up": "GPIO_MODE_IN_PU",
+        "geehy.gpio_pull_down": "GPIO_MODE_IN_PD",
+        "geehy.gpio_pull_no": "GPIO_MODE_IN_FLOATING",
     }
     analog_mode_map = {
-        "std.gpio_mode_analog": "GPIO_MODE_ANALOG",
+        "geehy.gpio_mode_analog": "GPIO_MODE_ANALOG",
     }
 
     pin_name = f'P{port.replace("GPIO", "")}{pin.replace("GPIO_PIN_", "")}'
-    function = project["Config"]["GPIO"][pin_name]["Function"]
-    gpio_config = project["Config"]["GPIO"][pin_name]["FunctionProperty"]["GPIO"]
+    function = __pin_config(project).get(pin_name, {}).get("signal", "")
+    gpio_config = __gpio_config(project).get(pin_name, {})
 
     mode = None
 
     if function == "GPIO-Output":
-        mode = output_mode_map[gpio_config["std.gpio_output_type_t"]]
+        mode = output_mode_map[gpio_config["geehy.gpio_output_type_t"]]
     elif function == "GPIO-Analog":
-        mode = analog_mode_map[gpio_config["std.gpio_mode_t"]]
+        mode = analog_mode_map[gpio_config["geehy.gpio_mode_t"]]
     elif function == "GPIO-Input":
-        mode = input_mode_map[gpio_config["std.gpio_pull_t"]]
+        mode = input_mode_map[gpio_config["geehy.gpio_pull_t"]]
     elif function == "GPIO-EXTI":
-        mode = input_mode_map[gpio_config["std.gpio_pull_t"]]
+        mode = input_mode_map[gpio_config["geehy.gpio_pull_t"]]
     else:
         mode = None
 
@@ -203,15 +209,15 @@ def gpio_pin_mode(project: dict, port: str, pin: str) -> bool:
 
 def gpio_pin_speed(project: dict, port: str, pin: str) -> bool:
     speed_map = {
-        "std.gpio_speed_low": "GPIO_SPEED_10MHz",
-        "std.gpio_speed_medium": "GPIO_SPEED_2MHz",
-        "std.gpio_speed_high": "GPIO_SPEED_50MHz",
+        "geehy.gpio_speed_low": "GPIO_SPEED_10MHz",
+        "geehy.gpio_speed_medium": "GPIO_SPEED_2MHz",
+        "geehy.gpio_speed_high": "GPIO_SPEED_50MHz",
     }
 
     pin_name = f'P{port.replace("GPIO", "")}{pin.replace("GPIO_PIN_", "")}'
-    gpio_config = project["Config"]["GPIO"][pin_name]["FunctionProperty"]["GPIO"]
+    gpio_config = __gpio_config(project).get(pin_name, {})
 
-    if not "std.gpio_speed_t" in gpio_config:
+    if not "geehy.gpio_speed_t" in gpio_config:
         return None
 
-    return speed_map[gpio_config["std.gpio_speed_t"]]
+    return speed_map[gpio_config["geehy.gpio_speed_t"]]
